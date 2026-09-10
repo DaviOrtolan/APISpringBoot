2e55129ed934325328f6548f56a2cd696ffbef05
Davi Mancini Ortolan <davimortolan@gmail.com>
2026-09-10 15:08:16 -0300

    Implementando consultas avançadas - configuração do Repository

--- src/main/java/com/produtoapi/repository/ProdutoRepository.java
+++ src/main/java/com/produtoapi/repository/ProdutoRepository.java
@@ -1,8 +1,14 @@
 package com.produtoapi.repository;
 
 import com.produtoapi.model.Produto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>  {
-
+	// Diferentes buscas
+	List<Produto> findByNome(String nome);
+	List<Produto> findByNomeContaining(String nome);
+	List<Produto> findByNomeAndStatus(String nome, String status);
+	List<Produto> findByNomeStartingWith(String prefix);
+	List<Produto> findByNomeEndingWith(String suffix);
 }
