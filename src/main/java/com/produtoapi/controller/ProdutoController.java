80d2e19328f8289c64568d5aa53e5b68b7552d36
Davi Mancini Ortolan <davimortolan@gmail.com>
2026-09-10 15:23:32 -0300

    Implementando consultas avançadas - configuração do Controller

--- src/main/java/com/produtoapi/controller/ProdutoController.java
+++ src/main/java/com/produtoapi/controller/ProdutoController.java
@@ -44,4 +44,30 @@ public class ProdutoController {
 	public Optional<Produto> findById(@PathVariable Long id) {
 		return produtoService.findById(id);
 	}
+	
+	// Endpoints de busca por nome
+	@GetMapping("/buscarPorNome")
+	public List<Produto> buscarPorNome(@RequestParam String valor) {
+		return produtoService.findByNome(valor);
+	}
+	
+	@GetMapping("/buscarPorNomeContendo")
+	public List<Produto> buscaPorNomeContendo(@RequestParam String valor) {
+		return produtoService.findByNomeContaining(valor);
+	}
+	
+	@GetMapping("/buscarPorNomeEStatus")
+	public List<Produto> buscarPorNomeEStatus(@RequestParam String nome, @RequestParam String status) {
+		return produtoService.findByNomeAndStatus(nome, status);
+	}
+	
+	@GetMapping("/buscarPorNomeComecandoCom")
+	public List<Produto> buscarPorNomeComecandoCom(@RequestParam String valor) {
+		return produtoService.findByNomeStartingWith(valor);
+	}
+	
+	@GetMapping("/buscarPorNomeTerminandoCom")
+	public List<Produto> buscarPorNomeTerminandoCom(@RequestParam String valor) {
+		return produtoService.findByNomeEndingWith(valor);
+	}
 }
