fa7b2a1f08ea16145693ee19274ef7f304ff26d3
Davi Mancini Ortolan <davimortolan@gmail.com>
2026-09-11 14:42:21 -0300

    Implementando consultas avançadas de Preço - configuração do Repository

--- src/main/java/com/produtoapi/repository/ProdutoRepository.java
+++ src/main/java/com/produtoapi/repository/ProdutoRepository.java
@@ -3,12 +3,22 @@ package com.produtoapi.repository;
 import com.produtoapi.model.Produto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>  {
-	// Diferentes buscas
+	// Diferentes buscas por NOME
 	List<Produto> findByNome(String nome);
 	List<Produto> findByNomeContaining(String nome);
 	List<Produto> findByNomeAndStatus(String nome, String status);
 	List<Produto> findByNomeStartingWith(String prefix);
 	List<Produto> findByNomeEndingWith(String suffix);
+	
+	// Diferentes buscas por PREÇO
+	List<Produto> findByPreco(Double preco);
+	List<Produto> findByPrecoGreaterThen(Double preco);
+	List<Produto> findByPrecoLessThan(Double preco);
+	
+	// usando Anottation @Query para obter o total de preços
+	@Query("SELECT SUM(p.preco) FROM Produto p")
+	Double findTotalPreco();
 }
